aa539dc56cdd17bcca8b55496c9c7f0dad96e3b6
soliken <kennethrex456@gmail.com>
2025-03-04 11:57:16 +0800

    UI Auth Overhaul

--- src/components/NavSidebar.jsx
+++ src/components/NavSidebar.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import { auth } from "../config/firebaseConfigs";
-import NavbarLoading from "./NavbarLoading";
 import { MdOutlineSpaceDashboard } from "react-icons/md";
 import { FaUsers } from "react-icons/fa6";
 import { CiSettings } from "react-icons/ci";
@@ -10,28 +9,17 @@ import { IoLogOutOutline } from "react-icons/io5";
 import { FaBook } from "react-icons/fa";
 import NavLoader from "../components/NavLoader";
 const NavSidebar = ({ userData }) => {
-  const [username, setUsername] = useState("");
-  const [userImg, setUserImg] = useState(null);
   const [isLoading, setisLoading] = useState(true);
 
   useEffect(() => {
-    const displayUser = async () => {
+    const awaitUserData = async () => {
       if (!userData) {
-        setUsername("");
-        setUserImg(null);
         return;
       }
-
-      if (userData.Username) {
-        setUsername(userData.Username);
-        setUserImg(userData.UserImg);
-        setisLoading(false);
-      } else {
-        setisLoading(false);
-      }
+      setisLoading(false);
     };
 
-    displayUser();
+    awaitUserData();
   }, [userData]);
 
   const logout = async () => {
@@ -46,12 +34,12 @@ const NavSidebar = ({ userData }) => {
     return <NavLoader />;
   }
   return (
-    <div className="w-[32vh] h-auto min-h-screen flex flex-col bg-gradient-to-b from-[#234faf] text-white to-[#152852]">
+    <div className="min-w-[28vh] max-w-[28vh] fixed h-screen flex flex-col bg-gradient-to-b from-[#234faf] text-white to-[#152852]">
       <div className="h-32 w-full flex flex-row justify-center items-center gap-2">
         <img src="/book.png" className="h-16 w-16 drop-shadow-lg" />
         <label className="font-semibold">MiManuTMS</label>
       </div>
-      <div className="flex-1 flex flex-col justify-between items-center px-12">
+      <div className="flex-1 flex flex-col justify-between px-8">
         <div className="flex flex-col gap-4">
           {userData?.UserRole === "Admin" ? (
             <>
@@ -89,6 +77,29 @@ const NavSidebar = ({ userData }) => {
             <IoLogOutOutline className="w-6 h-6" />
             <label className="cursor-pointer">Logout</label>
           </button>
+          <Link
+            to="/profile"
+            className="mt-5 w-full flex flex-row items-center gap-4 border-t border-white h-16"
+          >
+            <img
+              src={userData.UserImg}
+              className="w-10 h-10 object-cover rounded-full"
+            />
+            <div className="flex flex-col">
+              <label className="text-sm text-nowrap">{userData.Username}</label>
+              <label
+                className={`px-2 text-xs text-black rounded-xs ${
+                  userData.UserRole === "Admin"
+                    ? "bg-red-200"
+                    : userData.UserRole === "Trainor"
+                    ? "bg-amber-200"
+                    : "bg-sky-200"
+                }`}
+              >
+                {userData.UserRole}
+              </label>
+            </div>
+          </Link>
         </div>
       </div>
     </div>
