16c9745955a25f8f18ca7ff7d7bc69b0a7ddb3d3
soliken <kennethrex456@gmail.com>
2025-03-13 14:12:54 +0800

    Admin and Trainor, Employee Progress Viewing (Table not done)

---
+++ src/screens/Courses/Trainor/TrainorProgressList.jsx
@@ -0,0 +1,134 @@
+import React, { useEffect, useState } from "react";
+import fetchUser from "../../../hooks/get/fetchUser";
+import NavSidebar from "../../../components/NavSidebar";
+import { Link, useParams } from "react-router-dom";
+import fetchCourse from "../../../hooks/get/fetchCourse";
+import CourseSidebar from "../../../components/CourseSidebar";
+import fetchEnrolledEmployees from "../../../hooks/get/fetchEnrolledEmployees";
+const TrainorProgressList = ({ getUser }) => {
+  const { courseId } = useParams();
+  const [userData, setUserData] = useState(null);
+  const [courseData, setCourseData] = useState(null);
+  const [enrolledEmployees, setEnrolledEmployees] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const user = await fetchUser(getUser.uid);
+        setUserData(user);
+
+        const course = await fetchCourse(courseId);
+        setCourseData(course);
+
+        const enrolled = await fetchEnrolledEmployees(courseId);
+        setEnrolledEmployees(enrolled);
+        console.log(enrolled);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [courseId]);
+
+  const filteredEmployees = enrolledEmployees.filter((employee) =>
+    `${employee.FirstName} ${employee.LastName} ${employee.Email}`
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="flex h-full w-full flex-col md:flex-row md:pb-0 pb-20 poppins-normal">
+      <NavSidebar userData={userData} />
+      <div className="w-full flex flex-col gap-2 ps-66 h-auto min-h-screen p-12 bg-[#FAF9F6]">
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-col">
+            <label className="text-xl font-semibold">
+              {courseData?.CourseTitle}
+            </label>
+            <label className="text-sm text-gray-600">
+              {courseData?.CourseDescription}
+            </label>
+          </div>
+          <CourseSidebar userData={userData} />
+        </div>
+        <div className="w-full h-full flex flex-row mt-6">
+          <div className="flex-1 flex flex-col">
+            <div className="flex flex-row justify-between">
+              <label className="text-xl font-semibold">
+                Employee Progress List
+              </label>
+              <div className="flex flex-row gap-2">
+                <input
+                  type="text"
+                  placeholder="Search employee..."
+                  className="px-3 py-2 border rounded-lg"
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+              </div>
+            </div>
+            {/* Employees Table */}
+            <div className="w-full mt-5 bg-white shadow-md rounded-lg ">
+              <table className="w-full border-collapse">
+                <thead>
+                  <tr>
+                    <th className="py-3 px-6 text-left">Profile</th>
+                    <th className="py-3 px-6 text-left">Course Progress</th>
+                    <th className="py-3 px-6 text-left">Scores</th>
+                    <th className="py-3 px-6 text-left">Grade</th>
+                    <th className="py-3 px-6 text-left">Status</th>
+                    <th className="py-3 px-6 text-left">View</th>
+                  </tr>
+                </thead>
+                <tbody className="text-sm">
+                  {filteredEmployees.length > 0 ? (
+                    filteredEmployees.map((employee) => (
+                      <tr key={employee.id} className=" hover:bg-gray-100">
+                        <td className="py-3 px-6 flex flex-row items-center gap-4">
+                          <img
+                            src={employee.UserImg}
+                            alt="Profile"
+                            className="w-10 h-10 rounded-full object-cover"
+                          />
+                          {employee.FirstName} {employee.LastName}
+                        </td>
+                        <td className="py-3 px-6 text-gray-800">
+                          {employee.Email}
+                        </td>
+                        <td className="py-3 px-6 text-gray-800">
+                          {new Date(
+                            employee.DateEnrolled?.seconds * 1000
+                          ).toLocaleDateString()}
+                        </td>
+                        <td className="py-3 px-6 text-gray-800">
+                          <Link
+                            to={`/tcourse/${courseId}/progress/${employee.UserID}`}
+                          >
+                            View
+                          </Link>
+                        </td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr>
+                      <td
+                        colSpan="3"
+                        className="py-3 px-6 text-center text-gray-500"
+                      >
+                        No employees enrolled yet
+                      </td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TrainorProgressList;
