956ad4006cf8417287b2f0b75187af7ef89b5e6e
soliken <kennethrex456@gmail.com>
2025-05-02 18:12:37 +0800

    Deadline Condition, Form Type Condition To Not Render After Answering, Disable Email on Edit

--- src/screens/Courses/Employee/EmployeeScreen.jsx
+++ src/screens/Courses/Employee/EmployeeScreen.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import LoadingScreen from "../../../components/LoadingScreen";
 import fetchUser from "../../../hooks/get/fetchUser";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -16,6 +15,8 @@ import fetchAllAvailableTasks from "../../../hooks/get/employee/fetchAllAvailabl
 import { MdTask } from "react-icons/md";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { FaArrowCircleRight } from "react-icons/fa";
+import { getDocs, collection, query, where } from "firebase/firestore";
+import { db } from "../../../config/firebaseConfigs";
 import MBTI from "../../../assets/MBTI.jpg";
 
 const EmployeeScreen = ({ getUser, onLogout }) => {
@@ -25,6 +26,10 @@ const EmployeeScreen = ({ getUser, onLogout }) => {
   const [employeeCourses, setEmployeeCourses] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [taskData, setTaskData] = useState(null);
+  const [formAnswers, setFormAnswers] = useState({
+    MBTIForm: false,
+    SelfForm: false,
+  });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -38,6 +43,25 @@ const EmployeeScreen = ({ getUser, onLogout }) => {
         const task = await fetchAllAvailableTasks();
         setTaskData(task);
 
+        const answersSnapshot = await getDocs(
+          query(
+            collection(db, "Form-Answers"),
+            where("answeredBy", "==", getUser.uid)
+          )
+        );
+
+        const answers = { MBTIForm: false, SelfForm: false };
+
+        answersSnapshot.forEach((doc) => {
+          const data = doc.data();
+          if (data.formType === "MBTIForm" && data.answered) {
+            answers.MBTIForm = true;
+          } else if (data.formType === "SelfForm" && data.answered) {
+            answers.SelfForm = true;
+          }
+        });
+
+        setFormAnswers(answers);
         setLoading(false);
       } catch (error) {
         console.error("Error:", error);
@@ -73,109 +97,132 @@ const EmployeeScreen = ({ getUser, onLogout }) => {
           />
           <div className="w-full min-h-96 flex flex-col gap-5 mt-5">
             {filteredEmployeeCourses.length > 0 ? (
-              filteredEmployeeCourses.map((course) => (
-                <div
-                  key={course.id}
-                  className=" h-38 flex flex-row gap-5 shadow-y rounded-xl"
-                >
+              filteredEmployeeCourses.map((course) => {
+                const deadlineReached = course.CourseEnd.toDate() < new Date(); // Check deadline
+
+                return (
                   <div
-                    className="rounded-s-lg h-full w-[5vh]"
-                    style={{ backgroundColor: course.CourseColor }}
-                  ></div>
-                  <div className="flex-1  py-4 flex flex-col">
-                    <label className="text-lg font-semibold">
-                      {course.CourseTitle}
-                    </label>
-                    <label className="text-sm text-gray-600">
-                      {course.CourseDescription}
-                    </label>
-                    <div className="flex flex-row gap-5">
-                      <div className="flex flex-row items-center gap-5 mt-2">
-                        {Array.isArray(course.CourseTags) &&
-                        course.CourseTags.length > 0 ? (
-                          course.CourseTags.map((tag, index) => (
-                            <div
-                              className="bg-blue-500 text-white text-xs px-3 py-1 rounded-full flex items-center gap-2"
-                              key={index}
-                            >
-                              {tag}
-                            </div>
-                          ))
+                    key={course.id}
+                    className={`h-38 flex flex-row gap-5 shadow-y rounded-xl ${
+                      deadlineReached ? "opacity-50 pointer-events-none" : ""
+                    }`}
+                  >
+                    <div
+                      className="rounded-s-lg h-full w-[5vh]"
+                      style={{ backgroundColor: course.CourseColor }}
+                    ></div>
+                    <div className="flex-1 py-4 flex flex-col">
+                      <label className="text-lg font-semibold">
+                        {course.CourseTitle}
+                      </label>
+                      <label className="text-sm text-gray-600">
+                        {course.CourseDescription}
+                      </label>
+                      <div className="flex flex-row gap-5">
+                        <div className="flex flex-row items-center gap-5 mt-2">
+                          {Array.isArray(course.CourseTags) &&
+                          course.CourseTags.length > 0 ? (
+                            course.CourseTags.map((tag, index) => (
+                              <div
+                                className="bg-blue-500 text-white text-xs px-3 py-1 rounded-full flex items-center gap-2"
+                                key={index}
+                              >
+                                {tag}
+                              </div>
+                            ))
+                          ) : (
+                            <p className="text-gray-500 text-xs mt-5">
+                              No Tags
+                            </p>
+                          )}
+                        </div>
+                      </div>
+                      <div className="mt-5 flex flex-row justify-between pe-8">
+                        <label className="text-sm text-gray-500">
+                          Ends on:{" "}
+                          {course.CourseEnd.toDate().toLocaleDateString()}
+                        </label>
+                        {!deadlineReached ? (
+                          <Link
+                            to={`/course/${course.id}`}
+                            className="text-sm flex flex-row gap-1 items-center"
+                          >
+                            View Course{" "}
+                            <MdKeyboardArrowRight className="w-5 h-5" />
+                          </Link>
                         ) : (
-                          <p className="text-gray-500 text-xs mt-5">No Tags</p>
+                          <span className="text-sm text-red-500 font-medium">
+                            Deadline Reached
+                          </span>
                         )}
                       </div>
                     </div>
-                    <div className="mt-5 flex flex-row justify-between pe-8">
-                      <label className="text-sm text-gray-500">
-                        Ends on:{" "}
-                        {course.CourseEnd.toDate().toLocaleDateString()}
-                      </label>
-                      <Link
-                        to={`/course/${course.id}`}
-                        className="text-sm flex flex-row gap-1 items-center"
-                      >
-                        View Course <MdKeyboardArrowRight className="w-5 h-5" />
-                      </Link>
-                    </div>
                   </div>
-                </div>
-              ))
+                );
+              })
             ) : (
               <label className="text-gray-500 poppins-normal">
                 No courses found.
               </label>
             )}
           </div>
-          <div className="flex flex-col gap-4">
-            <label className="text-2xl font-semibold poppins-normal">
-              Available Forms
-            </label>
-            <div className="flex flex-col gap-5 md:flex-row justify-between">
-              <div className="md:w-84 w-full flex flex-col items-end">
-                <div className="w-full h-28 gap-4 text-sm rounded-lg shadow-y flex">
-                  <img
-                    src="/selfassessment.jpg"
-                    className="h-full w-1/3 object-cover rounded-s-lg"
-                  />
-                  <div className="flex flex-col justify-evenly pe-0 md:pe-4">
-                    <label className="flex items-center">
-                      Take Your Self-Assessment Here!
-                    </label>
-                    <div className="w-full justify-end flex">
-                      <Link
-                        to={`/self-form/${getUser.uid}`}
-                        className="px-2 py-1 flex justify-center text-white rounded-sm bg-[#152852]"
-                      >
-                        Get Started
-                      </Link>
+
+          {!(formAnswers.MBTIForm && formAnswers.SelfForm) && (
+            <div className="flex flex-col gap-4">
+              <label className="text-2xl font-semibold poppins-normal">
+                Available Forms
+              </label>
+              <div className="flex flex-col gap-5 md:flex-row justify-between">
+                {!formAnswers.SelfForm && (
+                  <div className="md:w-84 w-full flex flex-col items-end">
+                    <div className="w-full h-28 gap-4 text-sm rounded-lg shadow-y flex">
+                      <img
+                        src="/selfassessment.jpg"
+                        className="h-full w-1/3 object-cover rounded-s-lg"
+                      />
+                      <div className="flex flex-col justify-evenly pe-0 md:pe-4">
+                        <label className="flex items-center">
+                          Take Your Self-Assessment Here!
+                        </label>
+                        <div className="w-full justify-end flex">
+                          <Link
+                            to={`/self-form/${getUser.uid}`}
+                            className="px-2 py-1 flex justify-center text-white rounded-sm bg-[#152852]"
+                          >
+                            Get Started
+                          </Link>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
-              <div className="md:w-84 w-full flex flex-col items-end">
-                <div className="w-full h-28 gap-4 text-sm rounded-lg shadow-y flex ">
-                  <img
-                    src={MBTI}
-                    className="h-full w-1/3 object-cover rounded-s-lg"
-                  />
-                  <div className="flex flex-col justify-evenly pe-0 md:pe-4">
-                    <label className="flex items-center">
-                      Take Your MBTI Assessment Here!
-                    </label>
-                    <div className="w-full justify-end flex">
-                      <Link
-                        to={`/mbti-form/${getUser.uid}`}
-                        className="px-2 py-1 flex justify-center text-white rounded-sm bg-[#152852]"
-                      >
-                        Get Started
-                      </Link>
+                )}
+
+                {!formAnswers.MBTIForm && (
+                  <div className="md:w-84 w-full flex flex-col items-end">
+                    <div className="w-full h-28 gap-4 text-sm rounded-lg shadow-y flex ">
+                      <img
+                        src={MBTI}
+                        className="h-full w-1/3 object-cover rounded-s-lg"
+                      />
+                      <div className="flex flex-col justify-evenly pe-0 md:pe-4">
+                        <label className="flex items-center">
+                          Take Your MBTI Assessment Here!
+                        </label>
+                        <div className="w-full justify-end flex">
+                          <Link
+                            to={`/mbti-form/${getUser.uid}`}
+                            className="px-2 py-1 flex justify-center text-white rounded-sm bg-[#152852]"
+                          >
+                            Get Started
+                          </Link>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
+                )}
               </div>
             </div>
-          </div>
+          )}
         </div>
 
         <div className="w-5/12 md:flex hidden flex-col gap-12 items-end justify-evenly mt-20">
